4eaeb2aff45ab667dc2d1151800a7d3adecf4b0e
unknown <eitan1112@gmail.com>
2019-09-21 00:23:45 +0300

    Change ! to .

--- src/app.js
+++ src/app.js
@@ -1,89 +1,92 @@
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const app = express()
+const app = express();
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Eitan Nargassi'
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Eitan Nargassi"
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Eitan Nargassi'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Eitan Nargassi"
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help Page',
-        name: 'Eitan Nargassi'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    name: "Eitan Nargassi"
+  });
+});
 
-app.get('/weather', (req, res) => {    
+app.get("/weather", (req, res) => {
+  const address = req.query.address;
+  if (!address) {
+    return res.send({
+      error: "You must provide an address."
+    });
+  }
 
-    const address = req.query.address
-    if (!address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
     }
 
-    geocode(address, (error, {latitude, longitude, location} = {}) => {
+    forecast(
+      latitude,
+      longitude,
+      (error, { summary, temperature, rain } = {}) => {
         if (error) {
-            return res.send({ error })
+          return res.send({ error });
         }
+        res.send({
+          location,
+          summary,
+          temperature,
+          rain
+        });
+      }
+    );
+  });
+});
 
-        forecast(latitude, longitude, (error, {summary, temperature, rain} = {}) => {
-            if(error) {
-                return res.send({ error })
-            }
-            res.send({
-                location,
-                summary,
-                temperature,
-                rain
-            })
-        })        
-    })    
-})
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    message: "Help article not found",
+    title: "404 Page",
+    name: "Eitan Nargassi"
+  });
+});
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        message: 'Help article not found',  
-        title: '404 Page',
-        name: 'Eitan Nargassi'      
-    })
-})
-
-app.get('*', (req, res) => {
-    res.render('404', {
-        message: 'Error 404 - Page not found',
-        title: '404 Page',
-        name: 'Eitan Nargassi'
-    })
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    message: "Error 404 - Page not found",
+    title: "404 Page",
+    name: "Eitan Nargassi"
+  });
+});
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
-})
+  console.log("Server is up on port 3000.");
+});
